3f88a053dbff72c5188d4b2e519a91eab38ec888
songmho <songmho@gmail.com>
2015-05-12 11:49:07 +0900

    first commit

---
+++ MFC_proj2/MFC_proj2.h
@@ -0,0 +1,40 @@
+
+// MFC_proj2.h : MFC_proj2 응용 프로그램에 대한 주 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'stdafx.h'를 포함합니다."
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CMFC_proj2App:
+// 이 클래스의 구현에 대해서는 MFC_proj2.cpp을 참조하십시오.
+//
+
+class CMFC_proj2App : public CWinAppEx
+{
+public:
+	CMFC_proj2App();
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+	UINT  m_nAppLook;
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFC_proj2App theApp;
