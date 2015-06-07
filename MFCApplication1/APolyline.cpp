8cf42bf38f11b82484df041ecf7388d0d0dc2d1c
서재원 <superjaewoni@gmail.com>
2015-06-07 22:57:57 +0900

    폴리 그리기 완성!!!2

---
+++ MFCApplication1/APolyline.cpp
@@ -0,0 +1,100 @@
+#include "stdafx.h"
+#include "APolyline.h"
+
+
+APolyline::APolyline()
+{
+
+	//여기서 temp의 역할은
+	//폴리라인의 특성상 라인의 끝점을 찍어냄으로써
+	//생성하는 것인데 점을 찍엇을때 앞의 라인의 끝점으로 
+	//설정해주고, 다시 뒷라인의 시작점으로 설정해주는 작업에서
+	//쓰려고 temp를 만들었다.
+
+	this->color_p = *(new COLORREF(RGB(0, 0, 0)));
+	this->thick = 0.5;
+	this->pattern = 0;
+	this->index = 0;
+}
+
+void APolyline::setColor(COLORREF rgb)
+{
+	this->color_p = rgb;
+}
+
+COLORREF APolyline::getColor()
+{
+	return this->color_p;
+}
+
+void APolyline::setThick(double thick)
+{
+	this->thick = thick;
+}
+double APolyline::getThick()
+{
+	return this->thick;
+}
+
+void APolyline::setPattern(int pattern)
+{
+	this->pattern = pattern;
+}
+int APolyline::getPattern()
+{
+	return this->pattern;
+}
+
+void APolyline::next(Point point)
+{
+	if (this->index == 0)
+	{
+		temp = Line(point.getX(), point.getY(), point.getX(), point.getY());
+		poly_array.Add(temp);
+		index = poly_array.GetSize();
+	}
+	else
+	{
+		poly_array[index - 1].setEnd_x(point.getX());
+		poly_array[index - 1].setEnd_y(point.getY());
+		temp.setStart_x(point.getX());
+		temp.setStart_y(point.getY());
+		poly_array.Add(temp);
+		index = poly_array.GetSize();
+	}
+}
+
+void APolyline::moveAll()
+{
+
+}
+
+void APolyline::eraseAt(int index, Point point)
+{
+
+}
+
+void APolyline::eraseAll()
+{
+	this->~APolyline();
+}
+
+APolyline::~APolyline()
+{
+
+}
+
+int APolyline::get_index()
+{
+	return this->index;
+}
+
+void APolyline::draw(CDC* dc)
+{
+	for (int i = 0; i < poly_array.GetSize()-1; i++){
+		dc->MoveTo(poly_array[i].getStart_x(),
+			poly_array[i].getStart_y());
+		dc->LineTo(poly_array[i].getEnd_x(),
+			poly_array[i].getEnd_y());
+	}
+}
---
+++ MFCApplication1/APolyline.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Line.h"
+
+class APolyline
+{
+public:
+	APolyline();
+	void setColor(COLORREF rgb);
+	COLORREF getColor();
+
+	void setThick(double thick);
+	double getThick();
+
+	void setPattern(int pattern);
+	int getPattern();
+
+	void next(Point point);
+	void moveAll();
+	void eraseAt(int index, Point point);
+	void eraseAll();
+	void draw(CDC* dc);
+
+	int get_index();
+
+public:
+	CArray <Line, Line&> poly_array;
+	Line temp;
+	COLORREF color_p;
+	double thick;
+	int pattern;
+	int index;
+	~APolyline();
+};
+
