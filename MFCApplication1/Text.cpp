bbc295673216954037eb24d1d0169ab47e8ea08b
songmho <songmho@gmail.com>
2015-06-08 00:14:15 +0900

    필요없는 파일 삭제 및 텍스트 클래스 수정

--- MFCApplication1/Text.cpp
+++ MFCApplication1/Text.cpp
@@ -9,17 +9,20 @@ Text::Text(){
 void Text::setStart_x(int x){
 	this->start_x = x;
 	rect.setStart_x(x);
+	r.left = x;
 }
 void Text::setStart_y(int y){
 	this->start_y = y;
-
 	rect.setStart_y(y);
+	r.top = y;
 }
 void Text::setEnd_x(int x){
 	this->end_x = x;
+	r.right = x;
 }
 void Text::setEnd_y(int y){
 	this->end_y = y;
+	r.bottom = y;
 }
 
 int Text::getStart_x(){
@@ -35,8 +38,21 @@ int Text::getEnd_y(){
 	return this->end_y;
 }
 
+void Text::setString(CString str){
+	this->s = str;
+}
+
+CString Text::getString(){
+	return this->s;
+}
+
 void Text::makeRect(CDC* dc, int x, int y){
 	this->rect.draw(dc, x, y);
+	r.bottom = y;
+	r.right = x;
+	CFont f;
+	//	f.CreateFontW();
+	dc->DrawText(_T(" "), &r, DT_LEFT);
 }
 
 void Text::setRect(ARectangle rect){
